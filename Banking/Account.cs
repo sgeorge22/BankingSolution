f47abbe505d1954ffbb535a0224d4ca6ad4e594b
Sarah George <sgeorge@tql.com>
2021-05-20 15:46:37 -0400

    Add project files.

---
+++ Banking/Account.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking
+{
+    class Account
+    {
+
+        //Below is the properties
+        public string AccountNumber { get; private set; }
+        public decimal Balance { get; private set; }
+        public string RoutingNumber { get; private set; }
+        public string Description { get; set; }
+
+        //Below is the methods
+        public bool Deposit(decimal amount)
+        { // now we are in the deposit method 
+            if (amount <= 0) // boolean to check first that the return wont produce a false
+            {
+                Console.WriteLine($"Amount must be greater than 0.");
+                return false;
+            }
+            Balance = Balance + amount; // Balance + amount will return the value to the left side of the =
+            return true;
+
+        }
+        public bool Withdraw(decimal amount)
+        {// we are now in the withdraw method
+            if (amount <= 0) // boolean to check first that the return wont produce a false
+            {
+                Console.WriteLine($"Amount must be greater than 0.");
+                return false;
+            }
+            if (amount > Balance)
+            {
+                Console.WriteLine($"Insufficient funds.");
+                return false;
+            }
+            
+            Balance = Balance - amount;
+            return true;
+        } // now tab to Program and create and instance of the above methods 
+
+        //now create a method to transfer between accounts
+        public bool Transfer(decimal amount, Account toAccount)//create a variable for the account you are going to transfer into
+        {
+            var success = Withdraw(amount); //here we created a variable for the Withdraw(amount) so that we could use it in a simplified bool expression
+                if(success == true)//bool expression that is using our variable success to check if the statement is true
+                {
+                toAccount.Deposit(amount);//having this in the statement would allow for the bool to only move to this if the bool is true
+                return true;
+                }
+            return false;
+            
+           
+
+        }
+       
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//creating an istance of our methods on the Account tab
+            var acc1 = new Account();
+            acc1.Deposit(500);
+            acc1.Withdraw(200);
+            acc1.Withdraw(600);
+            acc1.Deposit(-400);
+            Console.WriteLine($"Balance is {acc1.Balance}.");
+
+
+            var acc2 = new Account();
+            acc1.Transfer(1000, acc2);
+            Console.WriteLine($"Balance is {acc2.Balance}.");
+        }
+
+
+    }
+}
